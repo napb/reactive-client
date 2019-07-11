e0a2e55ff8ac27df02ca5185367747c4fce63e02
Nicolas Pastor <nicolas.pastor@globant.com>
2019-07-11 14:48:47 -0400

    init commit

---
+++ src/main/java/com/nico/reactive/reactiveclient/Controlador.java
@@ -0,0 +1,24 @@
+package com.nico.reactive.reactiveclient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/reactive-client")
+public class Controlador {
+
+    WebClient client = WebClient.create("http://localhost:8081");
+
+    @GetMapping("/getMonoClient")
+    public void get(){
+        Mono<ObjetoDominio> objetoDominioMono = client.get()
+                .uri("/obtenerMono")
+                .retrieve()
+                .bodyToMono(ObjetoDominio.class);
+
+        objetoDominioMono.subscribe(System.out::println);
+    }
+}
---
+++ src/main/java/com/nico/reactive/reactiveclient/ObjetoDominio.java
@@ -0,0 +1,23 @@
+package com.nico.reactive.reactiveclient;
+
+public class ObjetoDominio {
+
+    private String nombre;
+    private String valor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
